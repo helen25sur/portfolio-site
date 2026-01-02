2f4e9dcc3e904df012be8b9731d8dd2890b7f6ab
Olena Surilova <elen21surilova@gmail.com>
2026-01-02 19:02:33 +0100

    del: remove fontawesome.css and add svg-icons

--- scripts/ProjectItem.js
+++ scripts/ProjectItem.js
@@ -15,7 +15,8 @@ export const ProjectItem = ({ proj }) => {
       <li className="portfolio-item" >
         <img className="portfolio-screen" src={proj.imgSmall} alt={"Screenshot of " + proj.name} loading="lazy" width={"1181px"} height={"655px"} />
         <h3 className="portfolio-title" onClick={openCard}>{proj.name}
-          <i className="fa-solid fa-arrow-right"></i>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+          <path d="M502.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L402.7 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l370.7 0-105.4 105.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg>
         </h3>
         <p className="portfolio-descr">{
           proj.tech.map((tech, index) => (
