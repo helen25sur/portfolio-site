624f8e540ee43058e823757c01cb315474d3f834
Olena Surilova <elen21surilova@gmail.com>
2026-01-02 21:02:10 +0100

    fix: little changes

--- scripts/ProjectCard.js
+++ scripts/ProjectCard.js
@@ -54,7 +54,9 @@ export const ProjectCard = ({ proj, openCard }) => {
           ( <a className="portfolio-link-btn" href={proj.codeLink} target="_blank" rel="noopener noreferrer">
             GitHub code <i className="fa-brands fa-square-github"></i>
           </a> ) : null }
-          <button className="close-card-btn" onClick={openCard}><i className="fa-solid fa-x"></i></button> 
+          <button className="close-card-btn" onClick={openCard}>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z"/></svg>
+          </button> 
 
         </div>
       </dialog>
